f5565f0ff69c892185351e0d3275e8eed6dce9f2
Stéphane Manciot <stephane.manciot@ebiznext.com>
2018-05-06 09:08:06 +0200

    to fix warning

--- security/src/main/scala/org/softnetwork/security/actors/ActorsModule.scala
+++ security/src/main/scala/org/softnetwork/security/actors/ActorsModule.scala
@@ -4,7 +4,6 @@ import akka.actor.ActorRef
 import org.softnetwork.akka.actors.ActorSystemLocator
 import org.softnetwork.notification.actors.NotificationSupervisor
 import org.softnetwork.notification.handlers.NotificationHandler
-import org.softnetwork.security.handlers.DefaultGenerator
 
 /**
   * Created by smanciot on 22/03/2018.
